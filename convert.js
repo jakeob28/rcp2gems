67f67b1f51d5940eac2114bc77e3e86586e71059
Jake O'Brien <jake.connor.obrien@gmail.com>
2022-10-12 17:27:41 -0400

    FSAE: fix bad current readings

--- convert.js
+++ convert.js
@@ -5,20 +5,22 @@ const cleanHeaders = require('./clean-headers');
 const fillRows = require('./fill-rows');
 const writeLog = require('./write-itlog');
 const degreesToRadians = require('./degrees-to-radians');
+const fixBadCurrents = require('./fix-bad-currents');
 
 function convert(filename, output) {
-  return new Promise((resolve, reject) => {
-    fs.createReadStream(filename)
-      .pipe(cleanHeaders())
-      .pipe(csv())
-      .pipe(fillRows())
-      .pipe(degreesToRadians())
-      .pipe(writeLog())
-      .pipe(fs.createWriteStream(output))
-    
-      .on("error", error => reject(error))
-      .on("end", () => resolve());
-  });
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(filename)
+            .pipe(cleanHeaders())
+            .pipe(csv())
+            .pipe(fillRows())
+            .pipe(degreesToRadians())
+            .pipe(fixBadCurrents())
+            .pipe(writeLog())
+            .pipe(fs.createWriteStream(output))
+
+            .on("error", error => reject(error))
+            .on("end", () => resolve());
+    });
 }
 
 module.exports = convert;
